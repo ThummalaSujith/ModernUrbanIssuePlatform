e0018cb78f06f94fcbb05e5c58a389a6ae5a5cf7
ThummalaSujith <srt7@njit.edu>
2025-04-11 10:57:26 -0400

    created API to fetch comments data from Dynamodb when user post, update, delete, edit the comment

---
+++ src/services/commentApi.js
@@ -0,0 +1,41 @@
+const API_BASE = "https://5cc1lcitg9.execute-api.eu-west-2.amazonaws.com/prod";
+
+export const getComments = async () => {
+  const res = await fetch(`${API_BASE}/comments?postid=${postId}`);
+  const data = res.json();
+  return data;
+};
+
+export const createComment = async (commentData) => {
+  const res = await fetch(`${API_BASE}/comments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(commentData),
+  });
+
+  const data = await res.json();
+
+  return data.comment || data;
+};
+
+export const deleteComment = async (commentId) => {
+  await fetch(`${API_BASE}/comments/${commentId}`, {
+    method: "DELETE",
+  });
+};
+
+export const updateComment = async (commentId, newText) => {
+ const res= await fetch(`${API_BASE}/comments/${commentId}`, {
+    method: "PUT",
+    body: JSON.stringify({ text: newText }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+  });
+
+  const data = await res.json()
+  return data;
+};
