f414d665258fd33d66173a1d8bc1dbb5b171ba01
ThummalaSujith <srt7@njit.edu>
2025-04-16 12:25:16 -0400

    Created CommentSection

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const CommentSection = () => {
+import { getComments, createComment } from "../../services/commentApi";
+import { useState, useEffect } from "react";
+import CommentForm from "./CommentForm";
+const CommentSection = ({ postId, currentUser }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getComments(postId);
+      setComments(data);
+    }
+    fetchData();
+  }, [postId]);
+
+  const handleAddComment = async (newComment) => {
+    const added = await createComment(newComment);
+    setComments((prev) => [...prev, added]);
+  };
   return (
-    <div>
-      
-      
-    </div>
-  )
-}
+    <div className="px-4 space-y-4 mt-2">
+      <h2 className="text-[17px] font-bold font-mono">Comments</h2>
 
-export default CommentSection
+      <CommentForm postId={postId} onSubmit={handleAddComment} currentUser={currentUser}/>
+    </div>
+  );
+};
+
+export default CommentSection;
